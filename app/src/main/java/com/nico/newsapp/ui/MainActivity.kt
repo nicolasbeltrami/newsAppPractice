897c65b1fc78ff629dd4721a07926fcb2e7a1140
Nico Beltrami <nicolas.beltrami@gmail.com>
2020-05-10 18:17:03 -0400

    implementacion viewmodel en main activity

--- app/src/main/java/com/nico/newsapp/ui/MainActivity.kt
+++ app/src/main/java/com/nico/newsapp/ui/MainActivity.kt
@@ -2,17 +2,27 @@ package com.nico.newsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.nico.newsapp.R
+import com.nico.newsapp.db.ArticleDatabase
+import com.nico.newsapp.repository.NewsRepository
+import com.nico.newsapp.ui.viewModels.NewsViewModel
+import com.nico.newsapp.ui.viewModels.NewsViewModelProviderFactory
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel: NewsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val repository= NewsRepository(ArticleDatabase(this))
+        val viewModelProviderFactory = NewsViewModelProviderFactory(repository)
+        viewModel = ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
         bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
     }
 }
