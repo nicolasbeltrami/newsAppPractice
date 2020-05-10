15288fe2de6762b9c297ef0aa85fd7fdef20b450
Nico Beltrami <nicolas.beltrami@gmail.com>
2020-05-10 18:20:06 -0400

    viewmodel provider factory implementado

--- app/src/main/java/com/nico/newsapp/ui/viewModels/NewsViewModelProviderFactory.kt
+++ app/src/main/java/com/nico/newsapp/ui/viewModels/NewsViewModelProviderFactory.kt
@@ -1,4 +1,11 @@
 package com.nico.newsapp.ui.viewModels
 
-class NewsViewModelProviderFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.nico.newsapp.repository.NewsRepository
+
+class NewsViewModelProviderFactory(val newsRepository: NewsRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return NewsViewModel(newsRepository) as T
+    }
 }
