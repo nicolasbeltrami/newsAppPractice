cca13a4224ee137d6c80cf419e9c12fd52800ade
Nico Beltrami <nicolas.beltrami@gmail.com>
2020-05-10 17:26:46 -0400

    conversor de tipo de dato para room implementado

--- app/src/main/java/com/nico/newsapp/db/Converters.kt
+++ app/src/main/java/com/nico/newsapp/db/Converters.kt
@@ -1,4 +1,16 @@
 package com.nico.newsapp.db
 
+import androidx.room.TypeConverter
+import com.nico.newsapp.model.Source
+
 class Converters {
+    @TypeConverter
+    fun fromSource(source: Source): String {
+        return source.name
+    }
+
+    @TypeConverter
+    fun toSource(name: String): Source {
+        return Source(name, name)
+    }
 }
