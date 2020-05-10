c56555080716f4a4bf6db1531dfc3f11b07cd26a
Nico Beltrami <nicolas.beltrami@gmail.com>
2020-05-10 18:19:25 -0400

    viewmodel creado

--- app/src/main/java/com/nico/newsapp/ui/viewModels/NewsViewModel.kt
+++ app/src/main/java/com/nico/newsapp/ui/viewModels/NewsViewModel.kt
@@ -1,2 +1,8 @@
 package com.nico.newsapp.ui.viewModels
 
+import androidx.lifecycle.ViewModel
+import com.nico.newsapp.repository.NewsRepository
+
+class NewsViewModel(val newsRepository: NewsRepository) :ViewModel(){
+
+}
