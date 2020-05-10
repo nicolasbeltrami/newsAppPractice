9f4e5361f7b45b8bb79c66cca9937663173ae249
Nico Beltrami <nicolas.beltrami@gmail.com>
2020-05-10 16:40:30 -0400

    url base agregada

--- app/src/main/java/com/nico/newsapp/util/Constants.kt
+++ app/src/main/java/com/nico/newsapp/util/Constants.kt
@@ -3,5 +3,6 @@ package com.nico.newsapp.util
 class Constants {
     companion object {
         const val API_KEY = "bda87308da8a49c3800a9cd40920e6b7"
+        const val BASE_URL = "https://newsapi.org"
     }
 }
