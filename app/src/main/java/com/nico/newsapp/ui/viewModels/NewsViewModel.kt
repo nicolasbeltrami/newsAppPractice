c82828e98d5018832b312cb0a19081c306cc6853
Nico Beltrami <nicolas.beltrami@gmail.com>
2020-05-10 20:32:18 -0400

    metodos en viewmodel implementados

--- app/src/main/java/com/nico/newsapp/ui/viewModels/NewsViewModel.kt
+++ app/src/main/java/com/nico/newsapp/ui/viewModels/NewsViewModel.kt
@@ -1,8 +1,56 @@
 package com.nico.newsapp.ui.viewModels
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.nico.newsapp.model.NewsResponse
 import com.nico.newsapp.repository.NewsRepository
+import com.nico.newsapp.util.Resource
+import kotlinx.coroutines.launch
+import retrofit2.Response
 
-class NewsViewModel(val newsRepository: NewsRepository) :ViewModel(){
+class NewsViewModel(private val newsRepository: NewsRepository) :ViewModel(){
+
+    val breakingNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    var breakingNewsPage = 1
+
+    val searchNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    var searchNewsPage = 1
+
+    init {
+        getBreakingNews("us")
+    }
+
+    fun getBreakingNews(countryCode: String) = viewModelScope.launch {
+        breakingNews.postValue(Resource.Loading())
+        val response = newsRepository.getBreakingNews(countryCode, breakingNewsPage)
+        breakingNews.postValue(handleBreakingNewsResponse(response))
+
+    }
+
+    fun searchNews(searchQuery: String) = viewModelScope.launch {
+        searchNews.postValue(Resource.Loading())
+        val response = newsRepository.searchNews(searchQuery, searchNewsPage)
+        searchNews.postValue(handleSearchNewsResponse(response))
+    }
+
+    private fun handleBreakingNewsResponse(response: Response<NewsResponse>): Resource<NewsResponse> {
+        if (response.isSuccessful){
+            response.body()?.let {  resultResponse ->
+                return Resource.Success(resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
+
+    private fun handleSearchNewsResponse(response: Response<NewsResponse>): Resource<NewsResponse> {
+        if (response.isSuccessful){
+            response.body()?.let {  resultResponse ->
+                return Resource.Success(resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
+
 
 }
