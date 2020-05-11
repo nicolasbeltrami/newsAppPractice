f27ac7f53f16f992c24524e2532d30b3b9414314
Nico Beltrami <nicolas.beltrami@gmail.com>
2020-05-10 20:29:58 -0400

    obtener noticias y buscar noticias en repository

--- app/src/main/java/com/nico/newsapp/repository/NewsRepository.kt
+++ app/src/main/java/com/nico/newsapp/repository/NewsRepository.kt
@@ -1,6 +1,14 @@
 package com.nico.newsapp.repository
 
+import com.nico.newsapp.api.RetrofitInstance
 import com.nico.newsapp.db.ArticleDatabase
 
-class NewsRepository(val db: ArticleDatabase) {
+class NewsRepository(
+    val db: ArticleDatabase
+) {
+    suspend fun getBreakingNews(countryCode: String, pageNumber: Int) =
+        RetrofitInstance.api.getBreakingNews(countryCode, pageNumber)
+
+    suspend fun searchNews(searchQuery: String, pageNumber: Int) =
+        RetrofitInstance.api.searchNews(searchQuery, pageNumber)
 }
