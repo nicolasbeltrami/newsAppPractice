925d2acb17bfedeef3c4e0d90428e714373def5f
Nico Beltrami <nicolas.beltrami@gmail.com>
2020-05-10 20:31:12 -0400

    Vista de ultimas noticias implementadas

--- app/src/main/java/com/nico/newsapp/ui/fragments/BreakingNewsFragment.kt
+++ app/src/main/java/com/nico/newsapp/ui/fragments/BreakingNewsFragment.kt
@@ -1,14 +1,20 @@
 package com.nico.newsapp.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.nico.newsapp.R
 import com.nico.newsapp.ui.MainActivity
+import com.nico.newsapp.ui.adapters.NewsAdapter
 import com.nico.newsapp.ui.viewModels.NewsViewModel
+import com.nico.newsapp.util.Resource
+import kotlinx.android.synthetic.main.fragment_breaking_news.*
 
 /**
  * A simple [Fragment] subclass.
@@ -16,6 +22,9 @@ import com.nico.newsapp.ui.viewModels.NewsViewModel
 class BreakingNewsFragment : Fragment() {
 
     lateinit var viewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
+
+    val TAG = "BreakingNewsFragment"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,6 +37,43 @@ class BreakingNewsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as MainActivity).viewModel
+        setupRecyclerView()
+
+        viewModel.breakingNews.observe(viewLifecycleOwner, Observer { response ->
+            when(response) {
+                is Resource.Success -> {
+                    hideProgressBar()
+                    response.data?.let { newsResponse ->
+                        newsAdapter.differ.submitList(newsResponse.articles)
+                    }
+                }
+                is Resource.Error -> {
+                    hideProgressBar()
+                    response.message?.let { message ->
+                        Log.i(TAG, "Ha ocurrido un error: $message")
+                    }
+                }
+                is Resource.Loading -> {
+                    showProgressBar()
+                }
+            }
+        })
+    }
+
+    private fun hideProgressBar() {
+        paginationProgressBar.visibility = View.INVISIBLE
+    }
+
+    private fun showProgressBar() {
+        paginationProgressBar.visibility = View.VISIBLE
+    }
+
+    private fun setupRecyclerView() {
+        newsAdapter = NewsAdapter()
+        rvBreakingNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
     }
 
 }
